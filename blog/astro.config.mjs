d983f75ca4965112f8d3fb99649d5f833de0d31e
Atlas-sudo <bbabacanbaba059@gmail.com>
2026-01-07 02:39:50 +0300

    modified:   .vscode/settings.json 	modified:   blog/astro.config.mjs 	modified:   blog/package-lock.json 	modified:   blog/package.json 	modified:   blog/server.mjs 	modified:   blog/src/pages/index.astro 	modified:   package-lock.json 	modified:   package.json 	modified:   the-sins-of-the-fathers/package-lock.json

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,26 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
-import tailwindcss from '@tailwindcss/vite'; 
-import node from "@astrojs/node"; 
+import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
 
 export default defineConfig({
-  site: 'https://blog.thesinsofthefathers.com', 
-  output: 'server', 
+  site: "https://blog.thesinsofthefathers.com",
+  output: "server",
   adapter: node({
-    mode: 'middleware' 
+    mode: "middleware",
   }),
   integrations: [
-    sanity({ 
-      projectId: "8cfeoaz2", 
+    sanity({
+      projectId: "8cfeoaz2",
       dataset: "production",
       useCdn: true,
-      apiVersion: "2023-05-03"
-    })
+      apiVersion: "2023-05-03",
+    }),
   ],
   vite: {
     plugins: [tailwindcss()],
     build: {
       chunkSizeWarningLimit: 1000,
-    }
+    },
   },
 });
