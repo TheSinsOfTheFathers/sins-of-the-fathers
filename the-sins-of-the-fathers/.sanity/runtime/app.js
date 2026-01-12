5c28b8d1919c6821953272d36b76a132ebc082f5
Barış Salih Babacan <barissalihbabacan@gmail.com>
2026-01-13 01:06:50 +0300

    Delete the-sins-of-the-fathers/.sanity/runtime directory

--- the-sins-of-the-fathers/.sanity/runtime/app.js
+++
@@ -1,9 +0,0 @@
-// This file is auto-generated on 'sanity dev'
-// Modifications to this file are automatically discarded
-import config from '../../sanity.config.js'
-import {renderStudio} from 'sanity'
-
-renderStudio(document.getElementById('sanity'), studioConfig, {
-  reactStrictMode: false,
-  basePath: '/',
-})
