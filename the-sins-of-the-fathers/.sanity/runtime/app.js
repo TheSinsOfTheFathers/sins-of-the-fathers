43d49d9eae611c98b36fd49e0c98f2249187e7b7
Barış Salih Babacan <barissalihbabacan@gmail.com>
2026-01-13 00:56:54 +0300

    modified:   blog/package-lock.json 	modified:   package-lock.json 	modified:   the-sins-of-the-fathers/.sanity/runtime/app.js 	modified:   the-sins-of-the-fathers/.sanity/runtime/index.html 	modified:   the-sins-of-the-fathers/package-lock.json 	modified:   the-sins-of-the-fathers/package.json 	modified:   the-sins-of-the-fathers/sanity.cli.ts

--- the-sins-of-the-fathers/.sanity/runtime/app.js
+++ the-sins-of-the-fathers/.sanity/runtime/app.js
@@ -1,9 +1,11 @@
+
 // This file is auto-generated on 'sanity dev'
 // Modifications to this file are automatically discarded
-import config from '../../sanity.config.js'
-import {renderStudio} from 'sanity'
+import studioConfig from "..\\..\\sanity.config.js"
+import {renderStudio} from "sanity"
 
-renderStudio(document.getElementById('sanity'), studioConfig, {
-  reactStrictMode: false,
-  basePath: '/',
-})
+renderStudio(
+  document.getElementById("sanity"),
+  studioConfig,
+  {reactStrictMode: false, basePath: "/"}
+)
